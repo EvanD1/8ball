2395a4b45e0440df4eaf7445a99a14e615011cf0
Evan Dalton <evand1@109d25lx1s8f8j7.local>
2019-02-08 13:38:46 -0700

    added IBOutlet and IBAction

--- Magic 8Ball/ViewController.swift
+++ Magic 8Ball/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+    
+    }
+    
     override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
